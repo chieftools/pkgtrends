561b3545b7d0b1449031beaa8cb5c2d3090a734f
Alex Bouma <me@alexbouma.me>
2019-01-22 22:18:42 +0100

    Little weekly email template updates

--- resources/views/emails/weekly.blade.php
+++ resources/views/emails/weekly.blade.php
@@ -8,10 +8,10 @@ The number below "last 7 days" compares to "4 weeks ago".
 <div id="deps">
 @component('mail::table')
 | Package | Last 7 days | Last week | 4 weeks ago |
-|:------- |:----------- |:--------- |:----------- |
+|:------- | -----------:| ---------:| -----------:|
 @foreach($deps as $dependency)
 @php($stats = $dependency['stats']->reverse()->values())
-| <a href="{{ $dependency['info']['permalink'] }}">{{ $dependency['info']['name'] }}</a><br><small>{{ $dependency['info']['source_formatted'] }}</small> | {{ $stats[0] }}<br><small style="color: {{ $stats[0] > $stats[4] ? '#28a745' : '#ffc107' }};">{{ $stats[0] > $stats[4] ? '+' : '' }}{{ $stats[0] - $stats[4] }}</small> | {{ $stats[1] }} | {{ $stats[4] }} |
+| <a href="{{ $dependency['info']['permalink'] }}">{{ $dependency['info']['name'] }}</a><br><small>{{ $dependency['info']['source_formatted'] }}</small> | {{ number_format($stats[0]) }}<br><small style="color: {{ $stats[0] > $stats[4] ? '#28a745' : '#ffc107' }};">{{ $stats[0] > $stats[4] ? '+' : '-' }}{{ abs(100 - (int)(100 * $stats[0] / $stats[4])) }}%</small> | {{ number_format($stats[1]) }}<br>&nbsp; | {{ number_format($stats[4]) }}<br>&nbsp; |
 @endforeach
 @endcomponent
 </div>
