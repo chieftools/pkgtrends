768daf8d4d61a00decf3d96b11259dec8fb2bdaa
MathijsKoning <mk@mathijskoning.nl>
2019-01-24 12:30:21 +0100

    Version bump

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
 
     'name'        => env('APP_NAME', 'Package Trends'),
     'title'       => 'Package Trends: Compare Packagist, PyPI, Hex, npm & WordPress package downloads',
-    'version'     => '0.3.0',
+    'version'     => '0.3.1',
     'description' => 'A quick way to compare package downloads across languages. Compare Packagist, PyPI, Hex, npm & WordPress package download statistics.',
 
     /*
