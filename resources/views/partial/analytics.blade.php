a2a212f28ca5257878c04e37214879074e25e88c
Alex Bouma <me@alexbouma.me>
2020-05-12 12:24:34 +0200

    Upgrade Fathom tracker to v2

--- resources/views/partial/analytics.blade.php
+++ resources/views/partial/analytics.blade.php
@@ -1,15 +1,5 @@
 @if(!config('app.debug') && config('app.analytics.fathom.siteId') !== null)
-    <script>
-        (function(f, a, t, h, o, m){
-            a[h]=a[h]||function(){
-                (a[h].q=a[h].q||[]).push(arguments)
-            };
-            o=f.createElement('script'),
-                m=f.getElementsByTagName('script')[0];
-            o.async=1; o.src=t; o.id='fathom-script';
-            m.parentNode.insertBefore(o,m)
-        })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
-        fathom('set', 'siteId', '{{ config('app.analytics.fathom.siteId') }}');
-        fathom('trackPageview');
-    </script>
+    <!-- Fathom - beautiful, simple website analytics -->
+    <script src="https://piranha.pkgtrends.app/script.js" site="{{ config('app.analytics.fathom.siteId') }}" defer></script>
+    <!-- / Fathom -->
 @endif
