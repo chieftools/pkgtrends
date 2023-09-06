06ea399d8856646ed9352c174f5c481feef2c92c
Alex Bouma <alex@bouma.me>
2023-09-06 20:32:32 +0200

    Version bump

--- config/app.php
+++ config/app.php
@@ -20,7 +20,7 @@ return [
         ? trim(file_get_contents($versionPath))
         : env('APP_VERSION', '@dev'),
 
-    'versionString' => env('APP_VERSION_STRING', '2023.3.1'),
+    'versionString' => env('APP_VERSION_STRING', '2023.3.2'),
 
     /*
     |--------------------------------------------------------------------------
