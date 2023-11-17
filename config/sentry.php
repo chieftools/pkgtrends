4fcacaf6c7e211f4f21a5e43268a34e59b7e43d5
Alex Bouma <alex@bouma.me>
2023-11-17 15:27:47 +0100

    Update Sentry config

--- config/sentry.php
+++ config/sentry.php
@@ -2,12 +2,27 @@
 
 return [
 
-    'dsn' => env('APP_DEBUG', false) ? null : env('SENTRY_DSN'),
+    'dsn' => env('APP_DEBUG', false) ? null : env('SENTRY_PRIVATE_DSN', env('SENTRY_LARAVEL_DSN')),
 
-    'release' => env('APP_DEBUG', false) ? '@develop' : config('app.version'),
+    'release' => env('APP_COMMIT_SHA', config('app.version')),
 
     'error_types' => E_ALL ^ E_DEPRECATED ^ E_USER_DEPRECATED,
 
+    'in_app_exclude' => [
+        base_path('vendor'),
+        app_path('Http/Middleware'),
+    ],
+
+    'in_app_include' => [
+        base_path('vendor/chieftools'),
+    ],
+
     'send_default_pii' => true,
+
+    'trace_propagation_targets' => [
+
+        'pkgtrends.app',
+
+    ],
 
 ];
