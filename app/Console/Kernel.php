4149294ad6022337d5b0c53c0343703e5468f3d3
Alex Bouma <alex@bouma.me>
2024-08-11 15:29:57 +0200

    Update logging configuration

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,91 +24,85 @@ class Kernel extends ConsoleKernel
         $this->scheduleQueue($schedule);
         $this->scheduleSubscriptions($schedule);
 
-        $schedule->command('cache:prune-stale-tags')->hourly();
+        $schedule->command('cache:prune-stale-tags')
+                 ->hourly()
+                 ->withoutOverlapping()->runInBackground()->onOneServer();
     }
 
     private function scheduleHex(Schedule $schedule): void
     {
         // Every day we import the daily download statistics
         $schedule->command(Commands\Import\Hex\Downloads::class)
-                 ->withoutOverlapping()
-                 ->runInBackground()
-                 ->onOneServer()
-                 ->dailyAt('04:00');
+                 ->dailyAt('04:00')
+                 ->withoutOverlapping()->runInBackground()->onOneServer()
+                 ->appendOutputTo($this->scheduleLogPath());
 
         // Every sunday we cleanup our internal package index / stats that are old or no longer in use
         $schedule->command(Commands\Import\Hex\Cleanup::class)
-                 ->withoutOverlapping()
-                 ->runInBackground()
-                 ->onOneServer()
-                 ->at('03:00')
-                 ->sundays();
+                 ->sundays()->at('03:00')
+                 ->withoutOverlapping()->runInBackground()->onOneServer()
+                 ->appendOutputTo($this->scheduleLogPath());
     }
 
     private function schedulePyPI(Schedule $schedule): void
     {
         // Every day we import the daily download statistics
         $schedule->command(Commands\Import\PyPI\Downloads::class)
-                 ->withoutOverlapping()
-                 ->runInBackground()
-                 ->onOneServer()
-                 ->dailyAt('04:15');
+                 ->dailyAt('04:15')
+                 ->withoutOverlapping()->runInBackground()->onOneServer()
+                 ->appendOutputTo($this->scheduleLogPath());
 
         // Every sunday we cleanup our internal package index / stats that are old or no longer in use
         $schedule->command(Commands\Import\PyPI\Cleanup::class)
-                 ->withoutOverlapping()
-                 ->runInBackground()
-                 ->onOneServer()
-                 ->at('03:15')
-                 ->sundays();
+                 ->sundays()->at('03:15')
+                 ->withoutOverlapping()->runInBackground()->onOneServer()
+                 ->appendOutputTo($this->scheduleLogPath());
 
         // Every saterday we update our internal package index with new and/or updated package descriptions
         $schedule->command(Commands\Import\PyPI\Packages::class)
-                 ->withoutOverlapping()
-                 ->runInBackground()
-                 ->at('06:00')
-                 ->saturdays();
+                 ->saturdays()->at('06:00')
+                 ->withoutOverlapping()->runInBackground()->onOneServer()
+                 ->appendOutputTo($this->scheduleLogPath());
     }
 
     private function scheduleQueue(Schedule $schedule): void
     {
         // Dispatches a job calling a ping hook so we know the queue is active
         $schedule->command(Commands\QueueHealthCheck::class)
-                 ->withoutOverlapping()
-                 ->runInBackground()
-                 ->onOneServer()
-                 ->everyMinute();
+                 ->everyMinute()
+                 ->withoutOverlapping()->runInBackground()->onOneServer()
+                 ->appendOutputTo($this->scheduleLogPath());
 
         // See: https://laravel.com/docs/9.x/horizon#metrics
         $schedule->command(SnapshotCommand::class)
-                 ->withoutOverlapping()
-                 ->runInBackground()
-                 ->onOneServer()
-                 ->everyFiveMinutes();
+                 ->everyFiveMinutes()
+                 ->withoutOverlapping()->runInBackground()->onOneServer()
+                 ->appendOutputTo($this->scheduleLogPath());
     }
 
     private function scheduleSubscriptions(Schedule $schedule): void
     {
         // Every monday we send the weekly reports to our subscribers
         $schedule->command(Commands\SendWeeklyReports::class)
-                 ->withoutOverlapping()
-                 ->runInBackground()
-                 ->onOneServer()
-                 ->at('08:00')
-                 ->mondays();
+                 ->mondays()->at('08:00')
+                 ->withoutOverlapping()->runInBackground()->onOneServer()
+                 ->appendOutputTo($this->scheduleLogPath());
 
         // Every day purge the unconfirmed subscriptions
         $schedule->command(Commands\PurgeSubscriptions::class)
-                 ->withoutOverlapping()
-                 ->runInBackground()
-                 ->onOneServer()
-                 ->dailyAt('01:00');
+                 ->dailyAt('01:00')
+                 ->withoutOverlapping()->runInBackground()->onOneServer()
+                 ->appendOutputTo($this->scheduleLogPath());
 
         // Every day purge the unsubscribed reports
         $schedule->command(Commands\PurgeReports::class)
-                 ->withoutOverlapping()
-                 ->runInBackground()
-                 ->onOneServer()
-                 ->dailyAt('01:15');
+                 ->dailyAt('01:15')
+                 ->withoutOverlapping()->runInBackground()->onOneServer()
+                 ->appendOutputTo($this->scheduleLogPath());
+    }
+
+    private function scheduleLogPath(): string
+    {
+        return config('logging.scheduled_commands_file');
     }
 }
--- config/logging.php
+++ config/logging.php
@@ -1,8 +1,6 @@
 <?php
 
-use Monolog\Handler\NullHandler;
-use Monolog\Handler\StreamHandler;
-use Monolog\Handler\SyslogUdpHandler;
+$logLevel = env('LOG_LEVEL', env('APP_ENV', 'production') === 'local' ? 'debug' : 'info');
 
 return [
 
@@ -11,90 +9,114 @@ return [
     | Default Log Channel
     |--------------------------------------------------------------------------
     |
-    | This option defines the default log channel that gets used when writing
-    | messages to the logs. The name specified in this option should match
-    | one of the channels defined in the "channels" configuration array.
+    | This option defines the default log channel that is utilized to write
+    | messages to your logs. The value provided here should match one of
+    | the channels present in the list of "channels" configured below.
     |
     */
 
     'default' => env('LOG_CHANNEL', 'stack'),
+
+    'scheduled_commands_file' => storage_path('logs/pkgtrends-schedule.log'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Deprecations Log Channel
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the log channel that should be used to log warnings
+    | regarding deprecated PHP and library features. This allows you to get
+    | your application ready for upcoming major versions of dependencies.
+    |
+    */
+
+    'deprecations' => [
+        'channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'),
+        'trace'   => env('LOG_DEPRECATIONS_TRACE', false),
+    ],
 
     /*
     |--------------------------------------------------------------------------
     | Log Channels
     |--------------------------------------------------------------------------
     |
-    | Here you may configure the log channels for your application. Out of
-    | the box, Laravel uses the Monolog PHP logging library. This gives
-    | you a variety of powerful log handlers / formatters to utilize.
+    | Here you may configure the log channels for your application. Laravel
+    | utilizes the Monolog PHP logging library, which includes a variety
+    | of powerful log handlers and formatters that you're free to use.
     |
-    | Available Drivers: "single", "daily", "slack", "syslog",
-    |                    "errorlog", "monolog",
-    |                    "custom", "stack"
+    | Available drivers: "single", "daily", "slack", "syslog",
+    |                    "errorlog", "monolog", "custom", "stack"
     |
     */
 
     'channels' => [
+
         'stack' => [
             'driver'            => 'stack',
-            'channels'          => ['daily'],
+            'channels'          => explode(',', env('LOG_STACK', 'single')),
             'ignore_exceptions' => false,
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path'   => storage_path('logs/laravel.log'),
-            'level'  => 'debug',
+            'driver'               => 'single',
+            'path'                 => storage_path('logs/pkgtrends-laravel.log'),
+            'level'                => $logLevel,
+            'replace_placeholders' => true,
+            ...env('APP_ENV', 'production') !== 'local' ? [
+                'formatter'      => Monolog\Formatter\JsonFormatter::class,
+                'formatter_with' => [
+                    'includeStacktraces' => true,
+                ],
+            ] : [],
         ],
 
         'daily' => [
-            'driver' => 'daily',
-            'path'   => storage_path('logs/laravel.log'),
-            'level'  => 'debug',
-            'days'   => 14,
-        ],
-
-        'slack' => [
-            'driver'   => 'slack',
-            'url'      => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
-            'emoji'    => ':boom:',
-            'level'    => 'critical',
-        ],
-
-        'papertrail' => [
-            'driver'       => 'monolog',
-            'level'        => 'debug',
-            'handler'      => SyslogUdpHandler::class,
-            'handler_with' => [
-                'host' => env('PAPERTRAIL_URL'),
-                'port' => env('PAPERTRAIL_PORT'),
-            ],
+            'driver'               => 'daily',
+            'path'                 => storage_path('logs/pkgtrends-laravel.log'),
+            'level'                => $logLevel,
+            'days'                 => env('LOG_DAILY_DAYS', 7),
+            'replace_placeholders' => true,
+            ...env('APP_ENV', 'production') !== 'local' ? [
+                'formatter'      => Monolog\Formatter\JsonFormatter::class,
+                'formatter_with' => [
+                    'includeStacktraces' => true,
+                ],
+            ] : [],
         ],
 
         'stderr' => [
-            'driver'    => 'monolog',
-            'handler'   => StreamHandler::class,
-            'formatter' => env('LOG_STDERR_FORMATTER'),
-            'with'      => [
+            'driver'     => 'monolog',
+            'level'      => $logLevel,
+            'handler'    => Monolog\Handler\StreamHandler::class,
+            'formatter'  => env('LOG_STDERR_FORMATTER'),
+            'with'       => [
                 'stream' => 'php://stderr',
             ],
+            'processors' => [Monolog\Processor\PsrLogMessageProcessor::class],
         ],
 
         'syslog' => [
-            'driver' => 'syslog',
-            'level'  => 'debug',
+            'driver'               => 'syslog',
+            'level'                => $logLevel,
+            'facility'             => env('LOG_SYSLOG_FACILITY', LOG_USER),
+            'replace_placeholders' => true,
         ],
 
         'errorlog' => [
-            'driver' => 'errorlog',
-            'level'  => 'debug',
+            'driver'               => 'errorlog',
+            'level'                => $logLevel,
+            'replace_placeholders' => true,
         ],
 
         'null' => [
             'driver'  => 'monolog',
-            'handler' => NullHandler::class,
+            'handler' => Monolog\Handler\NullHandler::class,
         ],
+
+        'emergency' => [
+            'path' => storage_path('logs/pkgtrends-laravel.log'),
+        ],
+
     ],
 
 ];
